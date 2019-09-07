060b0fa6313a20fd50f29aab996f73c153c776a8
rawanah995 <rawanah995@gmail.com>
2019-09-07 04:01:50 +0300

    adding html table assignment

--- flask_fundamentals/hello_flask/hello.py
+++ flask_fundamentals/hello_flask/hello.py
@@ -3,5 +3,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments!
+
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
 if __name__=="__main__":
     app.run(debug=True)
