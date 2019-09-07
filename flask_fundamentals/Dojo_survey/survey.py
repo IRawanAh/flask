7ceb5244a2d9c371b998329e4b857a9fcb2e600f
rawanah995 <rawanah995@gmail.com>
2019-09-07 05:06:15 +0300

    adding dojo survey assignment

---
+++ flask_fundamentals/Dojo_survey/survey.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect # added request
+app = Flask(__name__)          
+
+
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/result', methods=['POST'])
+def create_user():
+    return render_template("result.html", info=request.form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
